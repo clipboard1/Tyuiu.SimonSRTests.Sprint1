e22767190d8e53805db13ba422948dd256006909
clipboard1 <simon.serezha1@yandex.ru>
2023-10-09 00:02:30 +0500

    Update Task0

--- Tyuiu.SimonSRTests.Sprint1.Task0.V10.Lib/DataService.cs
+++ Tyuiu.SimonSRTests.Sprint1.Task0.V10.Lib/DataService.cs
@@ -1,6 +1,6 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
 
-namespace Tyuiu.SimonSRTests.Sprint1.Task0.V10.Lib
+namespace Tyuiu.SimonSRTests.Sprint1.Task0V10.Lib
 {
     public class DataService : ISprint1Task0V10
     {
