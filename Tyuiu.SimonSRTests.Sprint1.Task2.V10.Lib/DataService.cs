1d8452f99e2a89e640f466a091273fedb89d2bcf
clipboard1 <simon.serezha1@yandex.ru>
2023-10-09 00:31:53 +0500

    Added Task2

---
+++ Tyuiu.SimonSRTests.Sprint1.Task2.V10.Lib/DataService.cs
@@ -0,0 +1,9 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SimonSRTests.Sprint1.Task2.V10.Lib
+{
+    public class DataService : ISprint1Task2V10
+    {
+        
+    }
+}
