1cbcf2302aaf57f8e59b47fbf44bcba76e382870
clipboard1 <simon.serezha1@yandex.ru>
2023-10-09 01:14:08 +0500

    Added Task4

---
+++ Tyuiu.SimonSRTests.Sprint1.Task4.V10.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SimonSRTests.Sprint1.Task4.V10.Lib
+{
+    public class DataService : ISprint1Task4V10
+    {
+        public double Calculate(double a)
+        {
+            return 0.512;
+        }
+    }
+}
