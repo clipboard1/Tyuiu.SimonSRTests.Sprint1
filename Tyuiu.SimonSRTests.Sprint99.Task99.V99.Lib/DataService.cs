27daf1d06170dd7279b406fc7b76711e50fcf062
clipboard1 <simon.serezha1@yandex.ru>
2023-10-10 11:13:33 +0500

    Added task 99

---
+++ Tyuiu.SimonSRTests.Sprint99.Task99.V99.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint99;
+
+namespace Tyuiu.SimonSRTests.Sprint99.Task99.V99.Lib
+{
+    public class DataService : ISprint99Task99V99
+    {
+        public string GenerateTxtFile()
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\hello.txt";
+            StreamWriter f = new StreamWriter(path);
+            f.WriteLine("101");
+            f.Close();
+            return path;
+        }
+    }
+}
