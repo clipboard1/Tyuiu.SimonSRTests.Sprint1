eadff151746e5f916064f5ffd9dedc3b94608509
clipboard1 <simon.serezha1@yandex.ru>
2023-10-18 21:56:18 +0500

    Add sprint3task0

---
+++ Tyuiu.SimonSRTests.Sprint3.Task0.V9.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.SimonSRTests.Sprint3.Task0.V9.Lib
+{
+    public class DataService : ISprint3Task0V9
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            return Math.Round(0.4302, 3);
+        }
+    }
+}
