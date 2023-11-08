a3220860688f4439483738d22014dd354815d6d0
clipboard1 <simon.serezha1@yandex.ru>
2023-11-08 14:54:05 +0500

    Added task88

---
+++ Tyuiu.SimonSRTests.Sprint88.Task88.V88.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint88;
+
+namespace Tyuiu.SimonSRTests.Sprint88.Task88.V88.Lib
+{
+    public class DataService : ISprint88Task88V88
+    {
+        public string ReadTxtFile()
+        {
+            string result = System.IO.File.ReadAllText("C:\\TestFiles").Trim();
+            return result;
+        }
+    }
+}
