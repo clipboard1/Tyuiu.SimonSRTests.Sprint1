19385b8852303024a8b2a14e85c658ba973094f4
clipboard1 <simon.serezha1@yandex.ru>
2023-11-08 17:37:23 +0500

    Added task66

---
+++ Tyuiu.SimonSRTests.Sprint66.Task66.V66.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint77;
+
+namespace Tyuiu.SimonSRTests.Sprint66.Task66.V66.Lib
+{
+    public class DataService : ISprint66Task66V66
+    {
+        public string GenerateByteTxtFile()
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\hello.txt";
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                writer.Write("101");
+            }
+            return path;
+        }
+    }
+}
